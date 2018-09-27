f5943df6eef2e74c14fbc6b3418d9a529f9b5dba
Kenneth McKee <kcmthecomgeek@gmail.com>
2018-09-26 20:45:10 -0400

    Existing project upload.

---
+++ KCMText/Editor/EditorControl.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace KCMText.Editor
+{
+    partial class EditorControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtMain = new System.Windows.Forms.TextBox();
+            this.contextStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.selectAllMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.wordWrapMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.undoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontSelector = new System.Windows.Forms.FontDialog();
+            this.contextStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMain
+            // 
+            this.txtMain.ContextMenuStrip = this.contextStrip;
+            this.txtMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMain.Location = new System.Drawing.Point(0, 0);
+            this.txtMain.Multiline = true;
+            this.txtMain.Name = "txtMain";
+            this.txtMain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMain.Size = new System.Drawing.Size(284, 150);
+            this.txtMain.TabIndex = 0;
+            this.txtMain.WordWrap = false;
+            this.txtMain.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMain_KeyUp);
+            // 
+            // contextStrip
+            // 
+            this.contextStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoMenuItem,
+            this.redoMenuItem,
+            this.toolStripSeparator3,
+            this.cutMenuItem,
+            this.copyMenuItem,
+            this.pasteMenuItem,
+            this.toolStripSeparator1,
+            this.selectAllMenuItem,
+            this.fontMenuItem,
+            this.toolStripSeparator2,
+            this.wordWrapMenuItem,
+            this.aboutMenuItem});
+            this.contextStrip.Name = "contextStrip";
+            this.contextStrip.Size = new System.Drawing.Size(216, 242);
+            this.contextStrip.Opening += new System.ComponentModel.CancelEventHandler(this.contextStrip_Opening);
+            // 
+            // cutMenuItem
+            // 
+            this.cutMenuItem.Enabled = false;
+            this.cutMenuItem.Name = "cutMenuItem";
+            this.cutMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.cutMenuItem.Text = "C&ut";
+            // 
+            // copyMenuItem
+            // 
+            this.copyMenuItem.Enabled = false;
+            this.copyMenuItem.Name = "copyMenuItem";
+            this.copyMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.copyMenuItem.Text = "&Copy";
+            // 
+            // pasteMenuItem
+            // 
+            this.pasteMenuItem.Enabled = false;
+            this.pasteMenuItem.Name = "pasteMenuItem";
+            this.pasteMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.pasteMenuItem.Text = "&Paste";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(212, 6);
+            // 
+            // selectAllMenuItem
+            // 
+            this.selectAllMenuItem.Name = "selectAllMenuItem";
+            this.selectAllMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.selectAllMenuItem.Text = "&Select All";
+            this.selectAllMenuItem.Click += new System.EventHandler(this.selectAllMenuItem_Click);
+            // 
+            // fontMenuItem
+            // 
+            this.fontMenuItem.Name = "fontMenuItem";
+            this.fontMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.fontMenuItem.Text = "&Font...";
+            this.fontMenuItem.Click += new System.EventHandler(this.fontMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(212, 6);
+            // 
+            // wordWrapMenuItem
+            // 
+            this.wordWrapMenuItem.CheckOnClick = true;
+            this.wordWrapMenuItem.Name = "wordWrapMenuItem";
+            this.wordWrapMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.wordWrapMenuItem.Text = "&Word Wrap";
+            // 
+            // aboutMenuItem
+            // 
+            this.aboutMenuItem.Name = "aboutMenuItem";
+            this.aboutMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.aboutMenuItem.Text = "&About...";
+            this.aboutMenuItem.Click += new System.EventHandler(this.aboutMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(212, 6);
+            // 
+            // undoMenuItem
+            // 
+            this.undoMenuItem.Enabled = false;
+            this.undoMenuItem.Name = "undoMenuItem";
+            this.undoMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.undoMenuItem.Text = "&Undo... (not implemented)";
+            // 
+            // redoMenuItem
+            // 
+            this.redoMenuItem.Enabled = false;
+            this.redoMenuItem.Name = "redoMenuItem";
+            this.redoMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.redoMenuItem.Text = "&Redo... (not implemented)";
+            // 
+            // fontSelector
+            // 
+            this.fontSelector.FontMustExist = true;
+            // 
+            // EditorControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtMain);
+            this.Name = "EditorControl";
+            this.Size = new System.Drawing.Size(284, 150);
+            this.contextStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMain;
+        private System.Windows.Forms.ContextMenuStrip contextStrip;
+        private System.Windows.Forms.ToolStripMenuItem undoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem cutMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem selectAllMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem wordWrapMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutMenuItem;
+        private System.Windows.Forms.FontDialog fontSelector;
+    }
+}
---
+++ KCMText/Editor/EditorControl.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KCMText.Editor
+{
+    public partial class EditorControl : UserControl
+    {
+        /// <summary>
+        /// Occurs when the cursor has moved.
+        /// </summary>
+        public event EventHandler CursorPositionChanged;
+        /// <summary>
+        /// Occurs when a modifier key has been pressed (CAPS, NUMLOCK, or SCROLL).
+        /// </summary>
+        public event KeyEventHandler ModifierKeyPressed;
+        /// <summary>
+        /// Occurs when an about dialog has been requested. (About item selected in context menu).
+        /// </summary>
+        public event EventHandler AboutDialogRequested;
+
+        /// <summary>
+        /// Create a new default instance.
+        /// </summary>
+        public EditorControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Determines whether data can be cut from the control.
+        /// </summary>
+        public bool CanCut
+        {
+            get {if (txtMain.SelectionLength == 0) { return false; } else { return true; }}
+        }
+        /// <summary>
+        /// Determines whether data can be copied from the control.
+        /// </summary>
+        public bool CanCopy
+        {
+            get { if (txtMain.SelectionLength == 0) { return false; } else { return true; } }
+        }
+        /// <summary>
+        /// Determines whether data can be pasted into the control.
+        /// </summary>
+        public bool CanPaste
+        {
+            get { return Clipboard.ContainsText(); }
+        }
+        /// <summary>
+        /// Gets whether Word Wrap is turned on. Can also be used to control the setting.
+        /// </summary>
+        public bool WordWrap
+        {
+            get { return txtMain.WordWrap; }
+            set { txtMain.WordWrap = value; }
+        }
+        /// <summary>
+        /// Perform a cut action on the currently selected text.
+        /// </summary>
+        public void Cut()
+        {
+            txtMain.Cut();
+        }
+        /// <summary>
+        /// Perform a copy action on the currently selected text.
+        /// </summary>
+        public void Copy()
+        {
+            txtMain.Copy();
+        }
+        /// <summary>
+        /// Perform a paste action at the current location in the control.
+        /// </summary>
+        public void Paste()
+        {
+            txtMain.Paste();
+        }
+        /// <summary>
+        /// Selects all text in the control.
+        /// </summary>
+        public void SelectAll()
+        {
+            txtMain.Focus();
+            txtMain.SelectAll();
+        }
+        /// <summary>
+        /// Determine and return the current line number where the cursor is located.
+        /// </summary>
+        /// <returns>An integer value (current line number).</returns>
+        public int GetLineNumber()
+        {
+            return txtMain.GetLineFromCharIndex(txtMain.GetFirstCharIndexOfCurrentLine()) + 1;
+        }
+        /// <summary>
+        /// Determine and return the current character in the current line where the cursor is located.
+        /// </summary>
+        /// <returns>An integer value (current character minus the first character on the current line).</returns>
+        public int GetCurrentChar()
+        {
+            return (txtMain.SelectionStart - txtMain.GetFirstCharIndexOfCurrentLine()) + 1;
+        }
+        /// <summary>
+        /// Requests and performs a font change on the editor.
+        /// </summary>
+        /// <returns></returns>
+        public bool RequestFontChange()
+        {
+            fontSelector.Font = txtMain.Font;
+            DialogResult result = fontSelector.ShowDialog(this.Parent);
+            if (result == DialogResult.OK)
+            {
+                txtMain.Font = fontSelector.Font;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Purposefully raise the "CursorPositionModified" event.
+        /// </summary>
+        /// <param name="sender">Sender of the event.</param>
+        /// <param name="e">Basic event arguments.</param>
+        protected virtual void RaiseCursorPositionModified(object sender, EventArgs e)
+        {
+            if (CursorPositionChanged != null)
+                CursorPositionChanged(sender, e);
+        }
+
+        /// <summary>
+        /// Purposefully raise the "ModifierKeyPressed" event.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">Event arguments containing which keys were pressed.</param>
+        protected virtual void RaiseModifierKeyPressed(object sender, KeyEventArgs e)
+        {
+            if (ModifierKeyPressed != null)
+                ModifierKeyPressed(sender, e);
+        }
+
+        protected virtual void RaiseAboutDialogRequested(object sender, EventArgs e)
+        {
+            if (AboutDialogRequested != null)
+                AboutDialogRequested(sender, e);
+        }
+
+        /// <summary>
+        /// Catch when a key is pressed within the text box.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">Event arguments containing which keys were pressed.</param>
+        private void txtMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.CapsLock 
+                || e.KeyCode == Keys.NumLock 
+                || e.KeyCode == Keys.Scroll)
+            {
+                // A modifier key has been pressed. Raise the appropriate event.
+                RaiseModifierKeyPressed(sender, e);
+            }
+            // A key has been pressed. The cursor position has likely updated.
+            CursorPositionChanged(sender, e);
+        }
+        /// <summary>
+        /// Handle enabling/disabling of menu items before the menu is finished opening.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void contextStrip_Opening(object sender, CancelEventArgs e)
+        {
+            cutMenuItem.Enabled = CanCut;
+            copyMenuItem.Enabled = CanCopy;
+            pasteMenuItem.Enabled = CanPaste;
+            wordWrapMenuItem.Checked = WordWrap;
+        }
+        /// <summary>
+        /// Open the about dialog. Send event to parent.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            RaiseAboutDialogRequested(sender, e);
+        }
+        /// <summary>
+        /// User clicked the "Font..." menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fontMenuItem_Click(object sender, EventArgs e)
+        {
+            RequestFontChange();
+        }
+        /// <summary>
+        /// User clicked the "Select All" menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void selectAllMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+    }
+
+}
